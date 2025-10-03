0217894a69acb26a0a0813e57fb8878bba0964eb
Isuranga Vithanawasam <IsurangaVithanawasam@users.noreply.github.com>
2025-10-03 20:09:26 +0530

    added repository classes

--- src/main/java/com/isurangavithanawasam/hotelmanagementsystem/repo/UserRepository.java
+++ src/main/java/com/isurangavithanawasam/hotelmanagementsystem/repo/UserRepository.java
@@ -1,4 +1,13 @@
 package com.isurangavithanawasam.hotelmanagementsystem.repo;
 
-public interface UserRepository {
+import com.isurangavithanawasam.hotelmanagementsystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
